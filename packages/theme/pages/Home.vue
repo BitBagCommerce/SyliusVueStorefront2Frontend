1a3085c280d32ff08f15caa7822a704d7da57231
dead-playmod <playmod.pl@gmail.com>
2022-10-04 13:37:33 +0200

    add data from sylius to front page

---
+++ packages/theme/pages/Home.vue
@@ -0,0 +1,333 @@
+<template>
+  <div id="home">
+    <LazyHydrate when-idle>
+      <SfHero class="hero">
+        <SfHeroItem
+          v-for="(hero, i) in heroes"
+          :key="i"
+          :title="hero.title"
+          :subtitle="hero.subtitle"
+          :background="hero.background"
+          :image="hero.image"
+          :class="hero.className"
+        />
+      </SfHero>
+    </LazyHydrate>
+
+    <LazyHydrate when-visible>
+      <SfBannerGrid :banner-grid="1" class="banner-grid">
+        <template v-for="item in banners" v-slot:[item.slot]>
+          <SfBanner
+            :key="item.slot"
+            :title="item.title"
+            :subtitle="item.subtitle"
+            :description="item.description"
+            :button-text="item.buttonText"
+            :link="localePath(item.link)"
+            :image="item.image"
+            :class="item.class"
+          />
+        </template>
+      </SfBannerGrid>
+    </LazyHydrate>
+
+    <LazyHydrate when-visible>
+      <div class="similar-products">
+        <SfHeading title="Our products" :level="2"/>
+        <nuxt-link :to="localePath('/c/category/t-shirts')" class="smartphone-only">
+          {{ $t('See all') }}
+        </nuxt-link>
+      </div>
+    </LazyHydrate>
+
+    <LazyHydrate when-visible>
+        <SfCarousel class="carousel" :settings="{ peek: 16, breakpoints: { 1023: { peek: 0, perView: 2 } } }">
+          <template #prev="{go}">
+            <SfArrow
+              aria-label="prev"
+              class="sf-arrow--left sf-arrow--long"
+              @click="go('prev')"
+            />
+          </template>
+          <template #next="{go}">
+            <SfArrow
+              aria-label="next"
+              class="sf-arrow--right sf-arrow--long"
+              @click="go('next')"
+            />
+          </template>
+          <SfCarouselItem class="carousel__item" v-for="(product, i) in products" :key="i">
+            <SfProductCard
+              :title="product.title"
+              :image="product.image"
+              :regular-price="product.price.regular"
+              :max-rating="product.rating.max"
+              :score-rating="product.rating.score"
+              :show-add-to-cart-button="true"
+              wishlistIcon=""
+              isInWishlistIcon=""
+              :link="localePath({ name: 'home' })"
+              class="carousel__item__product"
+            />
+          </SfCarouselItem>
+        </SfCarousel>
+    </LazyHydrate>
+
+    <LazyHydrate when-visible>
+      <InstagramFeed />
+    </LazyHydrate>
+
+  </div>
+</template>
+<script>
+import {
+  SfHero,
+  SfBanner,
+  SfCallToAction,
+  SfSection,
+  SfCarousel,
+  SfProductCard,
+  SfImage,
+  SfBannerGrid,
+  SfHeading,
+  SfArrow,
+  SfButton
+} from '@storefront-ui/vue';
+import { computed, useContext } from '@nuxtjs/composition-api';
+import { useCategory } from '@vue-storefront/sylius';
+import { onSSR } from '@vue-storefront/core';
+import InstagramFeed from '~/components/InstagramFeed.vue';
+import LazyHydrate from 'vue-lazy-hydration';
+import cacheControl from './../helpers/cacheControl';
+import { useFacet, facetGetters, productGetters } from '@vue-storefront/sylius';
+
+export default {
+  name: 'Home',
+  middleware: cacheControl({
+    'max-age': 60,
+    'stale-when-revalidate': 5
+  }),
+  components: {
+    InstagramFeed,
+    SfHero,
+    SfBanner,
+    SfCallToAction,
+    SfSection,
+    SfCarousel,
+    SfProductCard,
+    SfImage,
+    SfBannerGrid,
+    SfHeading,
+    SfArrow,
+    SfButton,
+    LazyHydrate
+  },
+  setup() {
+    const { $config } = useContext();
+    const { categories } = useCategory('AppHeader:CategoryList');
+    const { result, search } = useFacet('category/t-shirts');
+    const productsRaw = computed(() => facetGetters.getProducts(result.value));
+
+    const products = computed(() => productsRaw.value.map(product => ({
+      title: product.name,
+      image: productGetters.getCoverImage(product),
+      price: {
+        regular: `${product.variants[0].channelPricings[0].price / 100} $`
+      },
+      rating: {
+        max: 5,
+        min: product.averageRating
+      },
+      isInWishlist: false
+    })));
+
+    const heroes = [
+      {
+        title: 'Colorful summer dresses are already in store',
+        subtitle: 'SUMMER COLLECTION 2019',
+        background: '#eceff1',
+        image: '/homepage/bannerH.webp'
+      },
+      {
+        title: 'Colorful summer dresses are already in store',
+        subtitle: 'SUMMER COLLECTION 2019',
+        background: '#efebe9',
+        image: '/homepage/bannerA.webp',
+        className:
+          'sf-hero-item--position-bg-top-left sf-hero-item--align-right'
+      },
+      {
+        title: 'Colorful summer dresses are already in store',
+        subtitle: 'SUMMER COLLECTION 2019',
+        background: '#fce4ec',
+        image: '/homepage/bannerB.webp'
+      }
+    ];
+
+    const banners = [
+      {
+        slot: 'banner-A',
+        subtitle: categories.value[0].name,
+        title: 'Cocktail & Party',
+        description:
+          'Find stunning women\'s cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses from all your favorite brands.',
+        buttonText: 'Shop now',
+        image: {
+          mobile: $config.theme.home.bannerA.image.mobile,
+          desktop: $config.theme.home.bannerA.image.desktop
+        },
+        class: 'sf-banner--slim desktop-only',
+        link: `/c/${categories.value[0].slug}`
+      },
+      {
+        slot: 'banner-B',
+        subtitle: categories.value[1].name,
+        title: 'Linen Dresses',
+        description:
+          'Find stunning women\'s cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses from all your favorite brands.',
+        buttonText: 'Shop now',
+        image: $config.theme.home.bannerB.image,
+        class: 'sf-banner--slim banner-central desktop-only',
+        link: `/c/${categories.value[1].slug}`
+      },
+      {
+        slot: 'banner-C',
+        subtitle: categories.value[2].name,
+        title: 'The Office Life',
+        image: $config.theme.home.bannerC.image,
+        class: 'sf-banner--slim banner__tshirt',
+        link: `/c/${categories.value[2].slug}`
+      },
+      {
+        slot: 'banner-D',
+        subtitle: categories.value[3].name,
+        title: 'Eco Sandals',
+        image: $config.theme.home.bannerD.image,
+        class: 'sf-banner--slim',
+        link: `/c/${categories.value[3].slug}`
+      }
+    ];
+
+    onSSR(() => search({
+      categorySlug: 'category/t-shirts',
+      channelsCode: 'FASHION_WEB',
+      attributes: {}
+    }));
+
+    return {
+      banners,
+      heroes,
+      products
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#home {
+  box-sizing: border-box;
+  padding: 0 var(--spacer-sm);
+  @include for-desktop {
+    max-width: 1240px;
+    padding: 0;
+    margin: 0 auto;
+  }
+}
+
+.hero {
+  margin: var(--spacer-xl) auto var(--spacer-lg);
+  --hero-item-background-position: center;
+  @include for-desktop {
+    margin: var(--spacer-xl) auto var(--spacer-2xl);
+  }
+  .sf-hero-item {
+    &:nth-child(even) {
+      --hero-item-background-position: left;
+      @include for-mobile {
+        --hero-item-background-position: 30%;
+        ::v-deep .sf-hero-item__subtitle,
+        ::v-deep .sf-hero-item__title {
+          text-align: right;
+          width: 100%;
+          padding-left: var(--spacer-sm);
+        }
+      }
+    }
+  }
+  ::v-deep .sf-hero__control {
+    &--right, &--left {
+      display: none;
+    }
+  }
+}
+
+.banner-grid {
+  --banner-container-width: 50%;
+  margin: var(--spacer-xl) 0;
+  ::v-deep .sf-link:hover {
+    color: var(--c-white);
+  }
+  @include for-desktop {
+    margin: var(--spacer-2xl) 0;
+    ::v-deep .sf-link {
+      --button-width: auto;
+      text-decoration: none;
+    }
+  }
+}
+
+.banner {
+  &__tshirt {
+    background-position: left;
+  }
+  &-central {
+    @include for-desktop {
+      --banner-container-flex: 0 0 70%;
+    }
+  }
+}
+
+.similar-products {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: var(--spacer-2xs);
+  --heading-padding: 0;
+  border-bottom: 1px var(--c-light) solid;
+  @include for-desktop {
+    border-bottom: 0;
+    justify-content: center;
+    padding-bottom: 0;
+  }
+}
+
+.call-to-action {
+  background-position: right;
+  margin: var(--spacer-xs) 0;
+  @include for-desktop {
+    margin: var(--spacer-xl) 0 var(--spacer-2xl) 0;
+  }
+}
+
+.carousel {
+    margin: 0 calc(0 - var(--spacer-sm)) 0 0;
+  @include for-desktop {
+    margin: 0;
+  }
+  &__item {
+    margin: 1.375rem 0 2.5rem 0;
+    @include for-desktop {
+      margin: var(--spacer-xl) 0 var(--spacer-xl) 0;
+    }
+    &__product {
+      --product-card-add-button-transform: translate3d(0, -15%, 0);
+    }
+  }
+  ::v-deep .sf-arrow--long .sf-arrow--right {
+    --arrow-icon-transform: rotate(180deg);
+     -webkit-transform-origin: center;
+     transform-origin: center;
+  }
+}
+
+</style>
