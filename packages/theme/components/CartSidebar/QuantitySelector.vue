b76311d40d8dea15778a7546eb2cb4e7fa46007f
Marcin Kukliński <senghe@gmail.com>
2022-11-28 21:22:20 +0100

    Fix styling in QuantitySelector

--- packages/theme/components/CartSidebar/QuantitySelector.vue
+++ packages/theme/components/CartSidebar/QuantitySelector.vue
@@ -9,6 +9,7 @@
         icon="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"
         viewBox="0 0 320 512"
         size="1rem"
+        color="white"
       />
     </SfButton>
 
@@ -23,6 +24,7 @@
         viewBox="0 0 512 512"
         icon="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
         size="1rem"
+        color="white"
       />
     </SfButton>
   </div>
@@ -109,18 +111,26 @@ export default {
     --input-padding: .25rem;
     --input-font-size: var(--font-size--xsm);
     text-align: center;
+    z-index: 1000 !important;
   }
 
   &__button {
-    padding: 0 .2rem;
+    padding: 0 .3rem;
     background-color: var(--c-light);
+    z-index: 0 !important;
 
     &--primary {
+      color: white !important;
       background-color: var(--c-primary);
+      border-top-right-radius: 5px;
+      border-bottom-right-radius: 5px;
     }
 
     &--danger {
+      color: white !important;
       background-color: var(--c-danger);
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 5px;
     }
 
     ::v-deep .sf-icon-path {
