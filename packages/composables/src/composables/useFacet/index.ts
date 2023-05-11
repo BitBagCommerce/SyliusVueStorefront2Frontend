ca5ba354e2f95e0dbbe296b015138e789595b365
Bartłomiej Sęk <bartlomiej.sek@bitbag.pl>
2023-05-11 09:21:16 +0200

    [vsf2-177] Fix category query, refactor category getters,

--- packages/composables/src/composables/useFacet/index.ts
+++ packages/composables/src/composables/useFacet/index.ts
@@ -1,29 +1,8 @@
-import { Context, useFacetFactory, FacetSearchResult, Logger } from '@vue-storefront/core';
+import { Context, useFacetFactory, FacetSearchResult } from '@vue-storefront/core';
 const factoryParams = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   search: async (context: Context, params: FacetSearchResult<any>) => {
-    let category = [];
-    let categories = [];
-    let categoriesFlat = [];
-
-    try {
-      const allCategories = await context.$sylius.api.getCategory();
-
-      const foundCategory = allCategories.filter(cat => cat.children.some(child => child.slug === params.input.categorySlug));
-      categories = foundCategory.length ? foundCategory : allCategories.filter(cat => cat.slug === params.input.categorySlug);
-      categoriesFlat = categories.reduce((acc, curr) => {
-        return acc.concat(curr.children);
-      }, categories);
-      category = categoriesFlat.find(cat => cat.slug === params.input.categorySlug);
-
-    } catch (e) {
-      Logger.error(e);
-    }
-
     return {
-      category,
-      categories,
-      categoriesFlat,
       page: params.input.page,
       availableFilters: [],
       availableSortingOptions: [
--- packages/composables/src/getters/categoryGetters.ts
+++ packages/composables/src/getters/categoryGetters.ts
@@ -1,23 +1,66 @@
-import { CategoryGetters, AgnosticCategoryTree } from '@vue-storefront/core';
+import { AgnosticBreadcrumb } from '@vue-storefront/core/lib/src/types';
+
 import { Category } from '@vue-storefront/sylius-api/src/types';
 
-const itemToTree = (category: Category): AgnosticCategoryTree => {
+const getTopLevelCategories = (categories: any[]): Category[] => {
+  return categories.filter(cat => cat.level === 1);
+};
+
+const getChildren = (category: any, categories: any[]): Category[] => {
+  return category
+    ? categories.filter(cat => cat.parent.id === category.id)
+    : categories.filter(cat => cat.level === 1);
+};
+
+const getParent = (category: any, categories: any[]) => {
+  if (!category)
+    return categories.find(cat => cat.level === 1);
+
+  return categories.find(cat => cat.id === category.parent.id);
+};
+
+const getTree = (current: Category, categories: Category[]) => {
+  if (!current || !categories)
+    return {
+      parent: null,
+      children: null
+    };
+
+  const children = getChildren(current, categories);
+
   return {
-    label: category.name,
-    slug: category.slug,
-    items: category.items.map(itemToTree),
-    isCurrent: false
+    parent: current,
+    children
   };
-};
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getCategoryTree = (category: Category): AgnosticCategoryTree => {
-  if (category) {
-    return itemToTree(category);
-  }
-  return {} as AgnosticCategoryTree;
+
 };
 
-export const categoryGetters: CategoryGetters<Category> = {
-  getTree: getCategoryTree
+const _buildBreadcrumbs = (categoryList: any[], allCategories): AgnosticBreadcrumb[] => {
+  const parent = getParent(categoryList[0], allCategories);
+
+  if (!parent || parent.level === 0)
+    return categoryList.map(category => ({
+      text: category.name,
+      link: `/c/${category.slug}`
+    }));
+
+  return _buildBreadcrumbs([parent, ...categoryList], allCategories);
 };
 
+const getBreadcrumbs = (current: Category, categories: Category[]): AgnosticBreadcrumb[] => {
+  if (current && categories)
+    return [
+      { text: 'Home', link: '/' },
+      ..._buildBreadcrumbs([current], categories)
+    ];
+
+  return [];
+};
+
+export const categoryGetters = {
+  getChildren,
+  getParent,
+  getTopLevelCategories,
+  getTree,
+  getBreadcrumbs
+};
