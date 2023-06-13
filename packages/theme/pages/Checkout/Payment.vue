36450f4a0a7ab68d3181d5da8e8a440f118f5132
Bartłomiej Sęk <bartlomiej.sek@bitbag.pl>
2023-06-13 14:38:29 +0200

    VSF2-115: Added cart reset after error with making order

--- packages/theme/pages/Checkout/Payment.vue
+++ packages/theme/pages/Checkout/Payment.vue
@@ -188,6 +188,7 @@ export default {
 
       if (makeError) {
         send({ type: 'danger', message: makeError.message });
+        setCart(null);
 
         router.push({ path: router.app.localePath('/') });
         return;
