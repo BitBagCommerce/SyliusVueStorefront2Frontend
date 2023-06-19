a88d4e39dff8a4f3fb60a434af767a49863b29bb
dead-playmod <playmod.pl@gmail.com>
2023-06-19 12:30:22 +0200

    vsf2-214: rewrite useCart composable

--- packages/composables/src/composables/useCart/index.ts
+++ packages/composables/src/composables/useCart/index.ts
@@ -1,198 +1,280 @@
+import { computed, Ref } from '@nuxtjs/composition-api';
 import {
+  sharedRef,
+  useVSFContext,
   Logger,
-  useCartFactory,
-  UseCartFactoryParams,
+  CustomQuery,
 } from '@vue-storefront/core';
-import type {
-  Cart,
-  CartItem,
-  Product,
-  Context,
-} from '@vue-storefront/sylius-api';
+import { Context, Product, Cart } from '@vue-storefront/sylius-api';
 
-const params: UseCartFactoryParams<Cart, CartItem, Product> = {
-  load: async (context: Context) => {
-    const apiState = context.$sylius.config.state;
-    let cartId = apiState.getCartId();
+export const useCart = () => {
+  const errorInit = {
+    load: null,
+    addItem: null,
+    removeItem: null,
+    updateItemQty: null,
+    clear: null,
+    removeCoupon: null,
+    isInCart: null,
+  };
+  const composableName = 'useCartFix';
+  const context = useVSFContext() as Context;
+  const cart = sharedRef(null, composableName) as Ref<Cart | null>;
+  const loading = sharedRef(false, `${composableName}-loading`) as Ref<boolean>;
+  const error = sharedRef(errorInit, `${composableName}-error`) as Ref<
+    typeof errorInit
+  >;
 
-    const createCart = async (): Promise<string> => {
-      const { cartToken } = await context.$sylius.api.createCart();
-      apiState.setCartId(cartToken);
-      return cartToken;
-    };
-
+  const handleCall = async <TReturn, TUpdate extends boolean>(
+    fn: () => Promise<TReturn>,
+    name: string,
+    update: TUpdate
+  ): Promise<TUpdate extends true ? void : TReturn> => {
     try {
-      // create new cart object in the backend
-      if (!cartId) cartId = await createCart();
+      loading.value = true;
 
-      const cartResponse = await context.$sylius.api.getCart(cartId);
+      const data = await fn();
 
-      // empty response means cart token is no longer valid
-      if (cartResponse && Object.keys(cartResponse).length === 0) {
-        cartId = await createCart();
-        return await context.$sylius.api.getCart(cartId);
+      if (update) {
+        cart.value = data as Cart;
+        error.value[name] = null;
+
+        return;
       }
 
-      return cartResponse;
+      return data as TUpdate extends true ? void : TReturn;
     } catch (e) {
-      Logger.error(e);
+      cart.value = null;
+      error.value[name] = e;
+
+      Logger.error(`${composableName}/${name}`, e);
+    } finally {
+      loading.value = false;
     }
+  };
 
-    return null;
-  },
-  addItem: async (context: Context, { product, quantity, customQuery }) => {
-    const apiState = context.$sylius.config.state;
-    let cartId = apiState.getCartId();
+  const load = () =>
+    handleCall(
+      async () => {
+        const apiState = context.$sylius.config.state;
+        let cartId = apiState.getCartId();
 
-    if (!cartId) {
-      await params.load(context, {});
-      cartId = apiState.getCartId();
-    }
+        const createCart = async (): Promise<string> => {
+          const { cartToken } = await context.$sylius.api.createCart();
+          apiState.setCartId(cartToken);
+          return cartToken;
+        };
 
-    const token = cartId.replace('/api/v2/shop/orders/', '');
+        // create new cart object in the backend
+        if (!cartId) cartId = await createCart();
 
-    if (Array.isArray(product)) {
-      const variants = product.map((prod) => ({
-        productVariant:
-          prod.selectedVariant.id ||
-          `/api/v2/shop/orders/${prod.selectedVariant.code}`,
-        quantity: prod.selectedVariant.quantity,
-      }));
-      const cart = await context.$sylius.api.addManyToCart(
-        {
-          variants,
-          token,
-        },
-        customQuery
-      );
+        const cartResponse = await context.$sylius.api.getCart(cartId);
 
-      return cart;
-    }
+        // empty response means cart token is no longer valid
+        if (cartResponse && Object.keys(cartResponse).length === 0) {
+          cartId = await createCart();
+          return await context.$sylius.api.getCart(cartId);
+        }
 
-    const variantId = product.selectedVariant.id;
-    const cart = await context.$sylius.api.addToCart(
-      {
-        quantity,
-        variantId,
-        token,
+        console.log('cartResponse', cartResponse);
+
+        return cartResponse;
       },
-      customQuery
+      'load',
+      true
     );
 
-    if ((cart as any).graphQLErrors?.length) {
-      throw {
-        message: (cart as any).graphQLErrors?.[0]?.debugMessage,
-      };
-    }
+  const addItem = ({
+    product,
+    quantity,
+    customQuery,
+  }: {
+    product: Product;
+    quantity: number;
+    customQuery: CustomQuery;
+  }) =>
+    handleCall(
+      async () => {
+        console.log('add cart');
+        const apiState = context.$sylius.config.state;
+        let cartId = apiState.getCartId();
 
-    return cart;
-  },
-  removeItem: async (context: Context, { product, customQuery }) => {
-    const apiState = context.$sylius.config.state;
-    const cartId = apiState.getCartId();
-    const cart = await context.$sylius.api.removeFromCart(
-      {
-        cartId,
-        itemId: String(product._id),
-      },
-      customQuery
-    );
-    return cart;
-  },
-  updateItemQty: async (
-    context: Context,
-    { product, quantity, customQuery }
-  ) => {
-    const apiState = context.$sylius.config.state;
-    const cartId = apiState.getCartId();
-    const cart = await context.$sylius.api.updateCartQuantity(
-      {
-        cartId,
-        itemId: String(product._id),
-        quantity,
-      },
-      customQuery
-    );
+        if (!cartId) {
+          await load();
+          cartId = apiState.getCartId();
+        }
 
-    if ((cart as any).graphQLErrors?.length) {
-      throw {
-        message: (cart as any).graphQLErrors?.[0]?.debugMessage,
-      };
-    }
+        const token = cartId.replace('/api/v2/shop/orders/', '');
 
-    return cart;
-  },
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  clear: async (context: Context) => {
-    const apiState = context.$sylius.config.state;
-    const cartId = apiState.getCartId();
-    return (await context.$sylius.api.clearCart({ cartId })) as any;
-  },
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  applyCoupon: async (
-    context: Context,
-    { currentCart, couponCode, customQuery }
-  ) => {
-    const apiState = context.$sylius.config.state;
-    const orderTokenValue = apiState
-      .getCartId()
-      .replace('/api/v2/shop/orders/', '');
-    const applyCouponResponse = await context.$sylius.api.addCouponToCart(
-      {
-        coupon: {
-          orderTokenValue,
-          couponCode,
-        },
+        if (Array.isArray(product)) {
+          const variants = product.map((prod) => ({
+            productVariant:
+              prod.selectedVariant.id ||
+              `/api/v2/shop/orders/${prod.selectedVariant.code}`,
+            quantity: prod.selectedVariant.quantity,
+          }));
+          const cart = await context.$sylius.api.addManyToCart(
+            {
+              variants,
+              token,
+            },
+            customQuery
+          );
+
+          return cart;
+        }
+
+        const variantId = product.selectedVariant.id;
+        const cart = await context.$sylius.api.addToCart(
+          {
+            quantity,
+            variantId,
+            token,
+          },
+          customQuery
+        );
+
+        if ((cart as any).graphQLErrors?.length) {
+          throw {
+            message: (cart as any).graphQLErrors?.[0]?.debugMessage,
+          };
+        }
+
+        return cart;
       },
-      customQuery
+      'addItem',
+      true
     );
 
-    if ((applyCouponResponse as any).graphQLErrors?.length) {
-      throw {
-        message: (applyCouponResponse as any).graphQLErrors?.[0]?.debugMessage,
-      };
-    }
+  const removeItem = ({
+    product,
+    customQuery,
+  }: {
+    product: Product;
+    customQuery: CustomQuery;
+  }) =>
+    handleCall(
+      async () => {
+        const apiState = context.$sylius.config.state;
+        const cartId = apiState.getCartId();
+        const cart = await context.$sylius.api.removeFromCart(
+          {
+            cartId,
+            itemId: String(product._id),
+          },
+          customQuery
+        );
 
-    return {
-      updatedCart: applyCouponResponse,
-      updatedCoupon: couponCode,
-    };
-  },
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  removeCoupon: async (
-    context: Context,
-    { currentCart, customQuery, couponCode }
-  ) => {
-    const apiState = context.$sylius.config.state;
-    const orderTokenValue = apiState
-      .getCartId()
-      .replace('/api/v2/shop/orders/', '');
-    const removeCouponResponse = await context.$sylius.api.removeCouponFromCart(
-      {
-        removeCouponInput: {
-          orderTokenValue,
-          couponCode,
-        },
+        return cart;
       },
-      customQuery
+      'removeItem',
+      true
     );
-    return {
-      updatedCart: removeCouponResponse,
-    };
-  },
-  isInCart: (context: Context, { currentCart, product }) => {
+
+  const updateItemQty = ({
+    product,
+    quantity,
+    customQuery,
+  }: {
+    product: Product;
+    quantity: number;
+    customQuery: CustomQuery;
+  }) =>
+    handleCall(
+      async () => {
+        const apiState = context.$sylius.config.state;
+        const cartId = apiState.getCartId();
+        const cart = await context.$sylius.api.updateCartQuantity(
+          {
+            cartId,
+            itemId: String(product._id),
+            quantity,
+          },
+          customQuery
+        );
+
+        if ((cart as any).graphQLErrors?.length) {
+          throw {
+            message: (cart as any).graphQLErrors?.[0]?.debugMessage,
+          };
+        }
+
+        return cart;
+      },
+      'updateItemQty',
+      true
+    );
+
+  const clear = () =>
+    handleCall(
+      async () => {
+        const apiState = context.$sylius.config.state;
+        const cartId = apiState.getCartId();
+
+        await context.$sylius.api.clearCart({ cartId });
+
+        return context.$sylius.api.getCart(cartId);
+      },
+      'clear',
+      true
+    );
+
+  const removeCoupon = ({
+    couponCode,
+    customQuery,
+  }: {
+    couponCode: string;
+    customQuery: CustomQuery;
+  }) =>
+    handleCall(
+      async () => {
+        const apiState = context.$sylius.config.state;
+        const orderTokenValue = apiState
+          .getCartId()
+          .replace('/api/v2/shop/orders/', '');
+        const removeCouponResponse =
+          await context.$sylius.api.removeCouponFromCart(
+            {
+              removeCouponInput: {
+                orderTokenValue,
+                couponCode,
+              },
+            },
+            customQuery
+          );
+
+        return removeCouponResponse;
+      },
+      'removeCoupon',
+      true
+    );
+
+  const isInCart = ({ product }: { product: Product }) => {
     if (Array.isArray(product)) return;
 
-    if (currentCart?.items) {
-      const productCheck = currentCart.items.filter(
-        (p) => product.selectedVariant.code === p.variant.code
+    if (cart.value?.items)
+      return cart.value.items.some(
+        (item) => product.selectedVariant.code === item.variant.code
       );
-      return productCheck.length > 0;
-    }
 
     return false;
-  },
-};
+  };
 
-export const useCart = useCartFactory<Cart, CartItem, Product>(params);
+  const setCart = (newCart: Cart) => {
+    cart.value = newCart;
+  };
+
+  return {
+    cart: computed(() => cart.value),
+    loading: computed(() => loading.value),
+    error: computed(() => error.value),
+    load,
+    addItem,
+    removeItem,
+    updateItemQty,
+    clear,
+    removeCoupon,
+    isInCart,
+    setCart,
+  };
+};
