eb9c9c95e8e12879f4d2acf2799121bb70e7c9d0
dawidFrankiewicz <dawid.frankiewicz2001@gmail.com>
2023-06-23 07:31:40 +0200

    Finished copy billing test

--- packages/theme/tests/e2e/integration/e2e-copy-billing.cy.ts
+++ packages/theme/tests/e2e/integration/e2e-copy-billing.cy.ts
@@ -1,7 +1,7 @@
 import page from '../pages/factory';
 
 before(() => {
-  cy.fixture('test-data/e2e-copy-billing').then((fixture) => {
+  cy.fixture('test-data/e2e-place-order').then((fixture) => {
     cy.fixtures = {
       data: fixture,
     };
@@ -26,14 +26,18 @@ context('Copy billing data to shipping form', () => {
     page.checkout.billing.fillForm(data.customer);
     page.checkout.billing.continueToShippingButton.click();
     page.checkout.shipping.heading.should('be.visible');
+
+    // Copy billing address
     cy.get('[data-testid="copyBillingAddress"] > label').click();
+
+    // Testing results
     page.checkout.shipping.firstName.should('have.value', data.customer.firstName);
-    // this.lastName.type(customer.lastName);
-    // this.streetName.type(customer.address.shipping.streetName);
-    // this.city.type(customer.address.shipping.city);
-    // this.country.select(customer.address.shipping.country);
-    // this.state.type(customer.address.shipping.state);
-    // this.zipcode.type(customer.address.shipping.zipcode);
-    // this.phone.type(customer.address.shipping.phone);
+    page.checkout.shipping.lastName.should('have.value', data.customer.lastName);
+    page.checkout.shipping.streetName.should('have.value', data.customer.address.billing.streetName);
+    page.checkout.shipping.city.should('have.value', data.customer.address.billing.city);
+    page.checkout.shipping.country.should('not.have.value', '');
+    page.checkout.shipping.state.should('have.value', data.customer.address.billing.state);
+    page.checkout.shipping.zipcode.should('have.value', data.customer.address.billing.zipcode);
+    page.checkout.shipping.phone.should('have.value', data.customer.address.billing.phone);
   });
 });
