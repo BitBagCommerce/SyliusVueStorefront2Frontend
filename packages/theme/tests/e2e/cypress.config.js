324f1943c562356785b839a81cd1ebd5507a9d0d
dawidFrankiewicz <dawid.frankiewicz2001@gmail.com>
2023-06-29 09:08:55 +0200

    Workflow test

--- packages/theme/tests/e2e/cypress.config.js
+++ packages/theme/tests/e2e/cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  fixturesFolder: 'fixtures',
+  fixturesFolder: 'packages/theme/tests/e2e/fixtures',
   viewportHeight: 1080,
   viewportWidth: 1920,
   screenshotOnRunFailure: true,
