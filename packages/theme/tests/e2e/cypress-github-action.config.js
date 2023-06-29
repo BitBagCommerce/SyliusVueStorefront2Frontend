743863fa36bc16b75e611e6996db9660e95f81ef
dawidFrankiewicz <dawid.frankiewicz2001@gmail.com>
2023-06-29 10:54:48 +0200

    Workflow test

---
+++ packages/theme/tests/e2e/cypress-github-action.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  fixturesFolder: 'packages/theme/tests/e2e/fixtures',
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  screenshotOnRunFailure: true,
+  screenshotsFolder: 'report/assets/screenshots',
+  video: false,
+  reporter: 'node_modules/mochawesome',
+  reporterOptions: {
+    reportDir: 'report',
+    reportFilename: 'report',
+    overwrite: false,
+    html: false,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('packages/theme/tests/e2e/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'packages/theme/tests/e2e/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
+  },
+});
