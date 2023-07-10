adc1eb6a53123ab8cc584cb7e3d21542bc792e21
Dawid Frankiewicz <69369874+DawidFrankiewicz@users.noreply.github.com>
2023-07-10 09:34:33 +0200

    VSF2-243 - Added automated e2e tests to github workflow with mocked api responses (#180)
    Squashed and merged changes from VSF2-243 branch

--- packages/theme/tests/e2e/integration/e2e-copy-billing.cy.ts
+++ packages/theme/tests/e2e/integration/e2e-copy-billing.cy.ts
@@ -15,20 +15,39 @@ context('Copy billing data to shipping form', () => {
     () => {
       const data = cy.fixtures.data;
 
+      // Mocking API responses
+      cy.interceptGql('getMinimalProduct', 'e2e-getMinimalProduct.json');
+      cy.interceptGql('getCategory', 'e2e-getCategory.json');
+      cy.interceptGql('createCart', 'e2e-createCart.json');
+      cy.interceptGql('getCart', 'e2e-getCart-empty.json');
+      cy.interceptGql('getFirstProductId', 'e2e-getFirstProductId.json');
+      cy.interceptGql('getProductAttribute', 'e2e-getProductAttribute.json');
+      cy.interceptGql('getCountries', 'e2e-getCountries.json');
+      cy.interceptGql('addToCart', 'e2e-addToCart.json');
+      cy.interceptGql('addAddress', 'e2e-addAddress-billing.json');
+      cy.interceptGql(
+        'getProductNotFiltered',
+        'e2e-getProductNotFiltered.json'
+      );
+
       // Add product to cart
       page.home.visit();
       page.home.header.categories.first().click();
+      cy.interceptGql('getCart', 'e2e-getCart-withProduct.json');
       page.category.addProductToCart();
       page.product.header.openCart();
       page.cart.goToCheckoutButton.click();
 
       // Fill in billing form
       page.checkout.billing.heading.should('be.visible');
+      cy.wait(1000);
       page.checkout.billing.fillForm(data.customer);
+      cy.interceptGql('getCart', 'e2e-getCart-billingSubmit.json');
       page.checkout.billing.continueToShippingButton.click();
 
       // Copy billing address
       page.checkout.shipping.heading.should('be.visible');
+      cy.wait(1000);
       page.checkout.shipping.copyBillingAddress.click();
 
       // Testing results
