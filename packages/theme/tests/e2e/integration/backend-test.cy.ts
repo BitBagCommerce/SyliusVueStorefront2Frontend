8f181b4e91b958285314360905133ae1d16c9933
dawidFrankiewicz <dawid.frankiewicz2001@gmail.com>
2023-07-20 12:30:52 +0200

    Changed urls for test

--- packages/theme/tests/e2e/integration/backend-test.cy.ts
+++ packages/theme/tests/e2e/integration/backend-test.cy.ts
@@ -1,13 +1,13 @@
 context('Try using backend', () => {
   it(['e2e', 'happypath'], 'Should successfully go to backend ulr', () => {
-    cy.visit('http://localhost:8080/', { failOnStatusCode: false });
+    cy.visit('http://localhost:8000/');
     cy.wait(10000);
   });
   it(
     ['e2e', 'happypath'],
     'Should successfully go to backend admin ulr',
     () => {
-      cy.visit('http://localhost:8080/admin', { failOnStatusCode: false });
+      cy.visit('http://localhost:8000/admin');
       cy.wait(10000);
     }
   );
