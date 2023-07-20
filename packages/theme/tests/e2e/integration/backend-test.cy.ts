701372f350e8a46244f5ac28b391b2cb171c463c
dawidFrankiewicz <dawid.frankiewicz2001@gmail.com>
2023-07-20 12:08:26 +0200

    Added argument to test

--- packages/theme/tests/e2e/integration/backend-test.cy.ts
+++ packages/theme/tests/e2e/integration/backend-test.cy.ts
@@ -1,13 +1,13 @@
 context('Try using backend', () => {
   it(['e2e', 'happypath'], 'Should successfully go to backend ulr', () => {
-    cy.visit('http://localhost:8080/');
+    cy.visit('http://localhost:8080/', { failOnStatusCode: false });
     cy.wait(10000);
   });
   it(
     ['e2e', 'happypath'],
     'Should successfully go to backend admin ulr',
     () => {
-      cy.visit('http://localhost:8080/admin');
+      cy.visit('http://localhost:8080/admin', { failOnStatusCode: false });
       cy.wait(10000);
     }
   );
