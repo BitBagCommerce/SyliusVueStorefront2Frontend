4edb97424cbd5bd47fa081bd26cecc4524f074de
dead-playmod <playmod.pl@gmail.com>
2023-07-20 11:49:21 +0200

    vsf2-297: add missing translations to user orders

--- packages/theme/lang/de.js
+++ packages/theme/lang/de.js
@@ -338,4 +338,14 @@ export default {
     'Es sind nicht so viele Artikel auf Lager',
   "Can't add less than one item to cart":
     'Es kann nicht weniger als ein Artikel in den Warenkorb gelegt werden',
+  'Payment State': 'Zahlungsstatus',
+  'Shipping State': 'Versandstaat',
+  myAddressesDescription:
+    'Verwalten Sie alle gewünschten Rechnungs-/Lieferadressen (Arbeitsplatz, Privatadresse usw.). Auf diese Weise müssen Sie die Lieferadresse nicht bei jeder Bestellung manuell eingeben.',
+  awaiting: 'warten',
+  paid: 'bezahlt',
+  refunded: 'zurückerstattet',
+  cancelled: 'abgesagt',
+  shipped: 'versendet',
+  ready: 'bereit',
 };
