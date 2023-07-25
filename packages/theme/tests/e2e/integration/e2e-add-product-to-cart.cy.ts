deb22ee5cf20bf03842b220d24a5883fc6df0ac0
dawidFrankiewicz <dawid.frankiewicz2001@gmail.com>
2023-07-25 08:05:28 +0200

    Updated test settings, improved data autogen

--- packages/theme/tests/e2e/integration/e2e-add-product-to-cart.cy.ts
+++ packages/theme/tests/e2e/integration/e2e-add-product-to-cart.cy.ts
@@ -1,195 +1,53 @@
 import page from '../pages/factory';
-import {
-  getCategory,
-  getMinimalProduct,
-  getFirstProductId,
-  getCart,
-  getProductAttribute,
-  addToCart,
-  getProductNotFiltered,
-  createCart,
-} from '../fixtures/api/e2e-api-responses';
-import { getCartModifications } from '../fixtures/api/e2e-api-responses-modifications';
 
 context('Adding products to cart', () => {
   it(['e2e', 'happypath'], 'Should successfully add product to cart', () => {
     const apiData = {};
-    cy.intercept('POST', '/api/sylius/**', () => {}).as('interceptedApiCall');
-
-    // let currentCart = getCart.empty;
-
-    // Mocking API responses
-    // cy.interceptApi('getMinimalProduct', getMinimalProduct.minimalProducts);
-    // cy.interceptApi('getCategory', getCategory.categories);
-    // cy.interceptApi('createCart', createCart.cart);
-    // cy.interceptApi('getCart', currentCart);
-    // cy.interceptApi('getFirstProductId', getFirstProductId.firstProductId);
-    // cy.interceptApi(
-    //   'getProductAttribute',
-    //   getProductAttribute.productAttributes
-    // );
-    // cy.interceptApi('addToCart', addToCart.singleProduct);
-    // cy.interceptApi(
-    //   'getProductNotFiltered',
-    //   getProductNotFiltered.productsNotFiltered
-    // );
+    cy.intercept('POST', '/api/sylius/**', (req) => {
+      // Regex gets everything to /sylius/ including /sylius/ and it is replaced with empty string
+      // Exaple url: http://localhost:8000/api/sylius/getCart
+      // This way we get only the data name (getCart, addProductToCart, etc.)
+      const dataName = req.url.replace(/.*\/sylius\//, '');
+      req.reply((res) => {
+        if (apiData[dataName] === undefined) {
+          apiData[dataName] = [res.body];
+        } else {
+          apiData[dataName].push(res.body);
+        }
+      });
+    });
 
     // Go to category page
     page.home.visit();
-
-    // cy.writeFile('fixtures/api/testdata.ts', 'const apiData = {');
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-    });
-
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-    });
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-    });
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-    });
-
     page.home.header.categories.first().click();
 
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-    });
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-    });
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-    });
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-    });
-
     // Add product
-    // cy.wait(10).then(() => {
-    //   currentCart = getCartModifications.addProduct(currentCart);
-    //   cy.interceptApi('addToCart', currentCart);
-    // });
-
     page.category.addProductToCart(0);
 
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-    });
-
     // Add product with quantity
-    // cy.wait(10).then(() => {
-    //   currentCart = getCartModifications.addProduct(currentCart, 12, 1);
-    //   cy.interceptApi('addToCart', currentCart);
-    // });
-
     page.category.addProductToCart(1, 12, true);
 
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-    });
-
-    // Add product with specific variant
-    // cy.wait(10).then(() => {
-    //   currentCart = getCartModifications.addProduct(
-    //     currentCart,
-    //     1,
-    //     2,
-    //     't_shirts',
-    //     [{ option: 't_shirt_size', id: 1 }]
-    //   );
-    //   cy.interceptApi('addToCart', currentCart);
-    // });
-
+    // Add product with specific variant=
     page.category.addProductToCart(2, 1, true, [
       {
         selectorId: 0,
         variant: 1,
-        expectedValue:
-          getMinimalProduct.minimalProducts.products[2].options[0].values[1]
-            .code,
       },
     ]);
 
-    cy.wait('@interceptedApiCall').then((interception) => {
-      const dataName = interception.request.url.replace(/.*\/sylius\//, '');
-      if (apiData[dataName] === undefined) {
-        apiData[dataName] = interception.response.body;
-        console.log('added');
-      } else {
-        apiData[dataName] = [apiData[dataName], interception.response.body];
-      }
-      cy.writeFile(
-        'fixtures/api/testdata.ts',
-        'const apiData = ' +
-          JSON.stringify(apiData) +
-          ';\n\nexport default apiData;'
-      );
-    });
-
     // Check cart sidebar content
     page.category.header.openCartSidebar();
+
+    cy.wait(4000).then(() => {
+      cy.writeFile(
+        'fixtures/api/e2e-add-product-to-cart.ts',
+        'const apiData = ' +
+          JSON.stringify(apiData) +
+          ';\n\nexport default apiData;\n'
+      ).then(() => {
+        console.log('Responses data file written!');
+        cy.wait(2000);
+      });
+    });
   });
 });
