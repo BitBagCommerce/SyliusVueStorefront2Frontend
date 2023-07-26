c056fd6aa161ca49bfd4b1c16b6baa98efb410e1
dawidFrankiewicz <dawid.frankiewicz2001@gmail.com>
2023-07-26 13:51:37 +0200

    Corrected test interceptions order, removed unused comments

--- packages/theme/tests/e2e/integration/e2e-add-product-to-cart.cy.ts
+++ packages/theme/tests/e2e/integration/e2e-add-product-to-cart.cy.ts
@@ -3,38 +3,30 @@ import apiData from '../fixtures/api/e2e-add-product-to-cart';
 
 context('Adding products to cart', () => {
   it(['e2e', 'happypath'], 'Should successfully add product to cart', () => {
-    // let apiDataGen = {};
-    // cy.dataAutogenIntercept(apiDataGen).then((newData) => {
-    //   apiDataGen = newData;
-    // });
-
-    // Intercepts
+    // Visit homepage
     cy.interceptApi('getCategory', apiData.getCategory[0]);
     cy.interceptApi('getMinimalProduct', apiData.getMinimalProduct[0]);
     cy.interceptApi('createCart', apiData.createCart[0]);
     cy.interceptApi('getCart', apiData.getCart[0]);
+    page.home.visit();
+
+    // Go to category page
     cy.interceptApi('getFirstProductId', apiData.getFirstProductId[0]);
     cy.interceptApi('getProductAttribute', apiData.getProductAttribute[0]);
     cy.interceptApi('getMinimalProduct', apiData.getMinimalProduct[0]);
     cy.interceptApi('getProductNotFiltered', apiData.getProductNotFiltered[0]);
-
-    // Go to category page
-    page.home.visit();
     page.home.header.categories.first().click();
 
     // Add product
     cy.interceptApi('addToCart', apiData.addToCart[0]);
-
     page.category.addProductToCart(0);
 
     // Add product with quantity
     cy.interceptApi('addToCart', apiData.addToCart[1]);
-
     page.category.addProductToCart(1, 12, true);
 
     // Add product with specific variant
     cy.interceptApi('addToCart', apiData.addToCart[2]);
-
     page.category.addProductToCart(2, 1, true, [
       {
         selectorId: 0,
@@ -44,7 +36,5 @@ context('Adding products to cart', () => {
 
     // Check cart sidebar content
     page.category.header.openCartSidebar();
-
-    // cy.dataAutogenSaveToFile(apiDataGen, 'e2e-add-product-to-cart');
   });
 });
