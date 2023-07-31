82799c74e6de0355c86a145fb47a89744ab5825b
dawidFrankiewicz <dawid.frankiewicz2001@gmail.com>
2023-07-31 12:19:04 +0200

    Separated add to cart by 'it' segments, generated data

--- packages/theme/tests/e2e/integration/e2e-add-product-to-cart.cy.ts
+++ packages/theme/tests/e2e/integration/e2e-add-product-to-cart.cy.ts
@@ -1,40 +1,115 @@
 import page from '../pages/factory';
-import apiData from '../fixtures/api/e2e-add-product-to-cart';
+import apiDataProduct from '../fixtures/api/e2e-add-product-to-cart/e2e-product';
+import apiDataQuantity from '../fixtures/api/e2e-add-product-to-cart/e2e-product-with-quantity';
+import apiDataVariant from '../fixtures/api/e2e-add-product-to-cart/e2e-product-with-variant';
 
 context('Adding products to cart', () => {
   it(['e2e', 'happypath'], 'Should successfully add product to cart', () => {
     // Visit homepage
-    cy.interceptApi('getCategory', apiData.getCategory[0]);
-    cy.interceptApi('getMinimalProduct', apiData.getMinimalProduct[0]);
-    cy.interceptApi('createCart', apiData.createCart[0]);
-    cy.interceptApi('getCart', apiData.getCart[0]);
+    cy.interceptApi('getCategory', apiDataProduct.getCategory[0]);
+    cy.interceptApi('getMinimalProduct', apiDataProduct.getMinimalProduct[0]);
+    cy.interceptApi('createCart', apiDataProduct.createCart[0]);
+    cy.interceptApi('getCart', apiDataProduct.getCart[0]);
     page.home.visit();
 
     // Go to category page
-    cy.interceptApi('getFirstProductId', apiData.getFirstProductId[0]);
-    cy.interceptApi('getProductAttribute', apiData.getProductAttribute[0]);
-    cy.interceptApi('getMinimalProduct', apiData.getMinimalProduct[1]);
-    cy.interceptApi('getProductNotFiltered', apiData.getProductNotFiltered[0]);
+    cy.interceptApi('getFirstProductId', apiDataProduct.getFirstProductId[0]);
+    cy.interceptApi(
+      'getProductAttribute',
+      apiDataProduct.getProductAttribute[0]
+    );
+    cy.interceptApi('getMinimalProduct', apiDataProduct.getMinimalProduct[1]);
+    cy.interceptApi(
+      'getProductNotFiltered',
+      apiDataProduct.getProductNotFiltered[0]
+    );
     page.home.header.categories.first().click();
 
     // Add product
-    cy.interceptApi('addToCart', apiData.addToCart[0]);
+    cy.interceptApi('addToCart', apiDataProduct.addToCart[0]);
     page.category.addProductToCart(0);
-
-    // Add product with quantity
-    cy.interceptApi('addToCart', apiData.addToCart[1]);
-    page.category.addProductToCart(1, 12, true);
-
-    // Add product with specific variant
-    cy.interceptApi('addToCart', apiData.addToCart[2]);
-    page.category.addProductToCart(2, 1, true, [
-      {
-        selectorId: 0,
-        variant: 1,
-      },
-    ]);
 
     // Check cart sidebar content
     page.category.header.openCartSidebar();
   });
+
+  it(
+    ['e2e', 'happypath'],
+    'Should successfully add product with quantity to cart',
+    () => {
+      // Visit homepage
+      cy.interceptApi('getCategory', apiDataQuantity.getCategory[0]);
+      cy.interceptApi(
+        'getMinimalProduct',
+        apiDataQuantity.getMinimalProduct[0]
+      );
+      cy.interceptApi('createCart', apiDataQuantity.createCart[0]);
+      cy.interceptApi('getCart', apiDataQuantity.getCart[0]);
+      page.home.visit();
+
+      // Go to category page
+      cy.interceptApi(
+        'getFirstProductId',
+        apiDataQuantity.getFirstProductId[0]
+      );
+      cy.interceptApi(
+        'getProductAttribute',
+        apiDataQuantity.getProductAttribute[0]
+      );
+      cy.interceptApi(
+        'getMinimalProduct',
+        apiDataQuantity.getMinimalProduct[1]
+      );
+      cy.interceptApi(
+        'getProductNotFiltered',
+        apiDataQuantity.getProductNotFiltered[0]
+      );
+      page.home.header.categories.first().click();
+
+      // Add product with quantity
+      cy.interceptApi('addToCart', apiDataQuantity.addToCart[0]);
+      page.category.addProductToCart(1, 12, true);
+
+      // Check cart sidebar content
+      page.category.header.openCartSidebar();
+    }
+  );
+
+  it(
+    ['e2e', 'happypath'],
+    'Should successfully add product wirth variant to cart',
+    () => {
+      // Visit homepage
+      cy.interceptApi('getCategory', apiDataVariant.getCategory[0]);
+      cy.interceptApi('getMinimalProduct', apiDataVariant.getMinimalProduct[0]);
+      cy.interceptApi('createCart', apiDataVariant.createCart[0]);
+      cy.interceptApi('getCart', apiDataVariant.getCart[0]);
+      page.home.visit();
+
+      // Go to category page
+      cy.interceptApi('getFirstProductId', apiDataVariant.getFirstProductId[0]);
+      cy.interceptApi(
+        'getProductAttribute',
+        apiDataVariant.getProductAttribute[0]
+      );
+      cy.interceptApi('getMinimalProduct', apiDataVariant.getMinimalProduct[1]);
+      cy.interceptApi(
+        'getProductNotFiltered',
+        apiDataVariant.getProductNotFiltered[0]
+      );
+      page.home.header.categories.first().click();
+
+      // Add product with specific variant
+      cy.interceptApi('addToCart', apiDataVariant.addToCart[0]);
+      page.category.addProductToCart(2, 1, true, [
+        {
+          selectorId: 0,
+          variant: 1,
+        },
+      ]);
+
+      // Check cart sidebar content
+      page.category.header.openCartSidebar();
+    }
+  );
 });
